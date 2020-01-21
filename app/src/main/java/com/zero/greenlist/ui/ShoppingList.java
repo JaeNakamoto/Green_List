db2ac330f973483c6460204f22c328199083c694
1337Alpha <jasonprikken00@gmail.com>
2020-01-21 01:07:19 +0100

    Added: -Settings button -Sort list button

--- app/src/main/java/com/zero/greenlist/ui/ShoppingList.java
+++ app/src/main/java/com/zero/greenlist/ui/ShoppingList.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -31,10 +33,35 @@ public class ShoppingList extends AppCompatActivity {
         textView.setText(listNaam);
 
         boodschappenlijst = new ArrayList<>();
-        Collections.addAll(boodschappenlijst, "product 1", "product 2", "Product 3");
-        boodschappenlijst.add("product 4");
+        Collections.addAll(boodschappenlijst, "product 1", "product 2", "product 3", "product 4", "product 5", "product 6", "product 7", "product 8", "product 9");
+        boodschappenlijst.add("product 10");
+        boodschappenlijst.add("product 11");
+        boodschappenlijst.add("product 12");
+        boodschappenlijst.add("product 13");
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, boodschappenlijst);
         lv = (ListView)  findViewById(R.id.listView);
         lv.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_shopping_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.action_settings) {
+            return true;
+        }
+
+        if(id == R.id.action_sort) {
+            Collections.sort(boodschappenlijst);
+            lv.setAdapter(adapter);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
